1d68e65f317be45267320044836b80ce3442d558
Anibal Santiago <anibsanmor@gmail.com>
2018-01-23 23:52:10 -0600

    update task

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,35 @@ var gulp = require('gulp');
 var exec = require('child_process').exec;
 gulp.task('icon2fonts', function () {
 
-	exec( 'mkdir -p dist/',
-  	function (error, sdtout, stderr) {
-  		if (error){
-  			console.log('exec mkdir with error::'+error);
-  			return;
-  		} else{
-  			console.log('directory ready!!!');
-  		}
-  	});
 
-  return exec( 'icon-font-generator icons/**/*.svg -o dist/',
-  	function (error, sdtout, stderr) {
-  		if (error){
-  			console.log('exec generator with error::'+error);
-  			return;
-  		} else{
-  			console.log('generation correct!!!');
-  		}
-  	});
+
+
+
+  return exec( 'npm install -g icon-font-generator',
+           	function (error, sdtout, stderr) {
+           		if (error){
+           			console.log('exec install with error::'+error);
+           			return;
+           		} else{
+           			console.log('install ready!!!');
+           			exec( 'mkdir -p dist/',
+                       	function (error, sdtout, stderr) {
+                       		if (error){
+                       			console.log('exec mkdir with error::'+error);
+                       			return;
+                       		} else{
+                       			console.log('directory ready!!!');
+                       			exec( 'icon-font-generator icons/**/*.svg -o dist/',
+                                   	function (error, sdtout, stderr) {
+                                   		if (error){
+                                   			console.log('exec generator with error::'+error);
+                                   			return;
+                                   		} else{
+                                   			console.log('generation correct!!!');
+                                   		}
+                                   	});
+                       		}
+                       	});
+           		}
+           	});
 });
